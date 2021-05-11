98cea50879d0b1eec5bfc1c4396b149d0355f9ac
bogdan-nd <bogdan6401@gmail.com>
2021-05-11 17:51:11 +0300

    feat: added script for pop-ups

---
+++ scripts/popUp.js
@@ -0,0 +1,7 @@
+function openPopUp(popUpId){
+    document.getElementById(popUpId).style.display = "block";
+}
+
+function closePopUp(popUpId){
+    document.getElementById(popUpId).style.display = "none";
+}
